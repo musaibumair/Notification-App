0ce9abd7d510a3386a70112f8cf4755601937a67
USER <ranamusaib5102000@gmail.com>
2024-04-30 11:20:20 +0500

    add the images in teacher

--- lib/screen/studentdashboard/eventdashboard.dart
+++ lib/screen/studentdashboard/eventdashboard.dart
@@ -21,24 +21,27 @@ class _EventDashBoardState extends State<EventDashBoard> {
       title: Text("Event Notification"),
     ),
       drawer: Drawer(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              padding: EdgeInsets.only(top: 20,bottom: 20),
-              color: Theme.of(context).canvasColor,
-              margin: EdgeInsets.only(top: 50,bottom: 30),
-
-              child:Column(children: [
-                Center(child: CircleAvatar(child: Icon(Icons.person,size: 30,),)),
-                Text(FirebaseAuth.instance.currentUser!.email.toString(),style: TextStyle(fontSize: 16),)
-              ],)
-          ),
-          InkWell(child: ListTile(leading: Icon(Icons.logout),title: Text("Log Out"),onTap: (){ FirebaseAuth.instance.signOut();
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>LoginScreen(),));},)),
-        ],
-      ),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                padding: EdgeInsets.only(top: 20,bottom: 20),
+                color: Theme.of(context).canvasColor,
+                margin: EdgeInsets.only(top: 50,bottom: 30),
+          
+                child:Column(children: [
+                  Center(child: CircleAvatar(child: Icon(Icons.person,size: 30,),)),
+                  Text(FirebaseAuth.instance.currentUser!.email.toString(),style: TextStyle(fontSize: 16),)
+                ],)
+            ),
+            InkWell(child: ListTile(leading: Icon(Icons.logout),title: Text("Log Out"),onTap: (){ FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>LoginScreen(),));},)),
+          ],
+                ),
+        ),
       ),
       body: FirebaseAnimatedList(
         query:databaseReference,
--- lib/screen/teacherdashboard/teacherdashboard.dart
+++ lib/screen/teacherdashboard/teacherdashboard.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:notificationapp/assets/widget/drawerwidget.dart';
 
 import '../login/loginscreen.dart';
@@ -14,13 +19,31 @@ class NotificationScreen extends StatefulWidget {
 
 class _NotificationScreenState extends State<NotificationScreen> {
   var databaseReference=FirebaseDatabase.instance.ref("Message");
-  TextEditingController message=TextEditingController();
+  TextEditingController messageController=TextEditingController();
+  ImagePicker imagePicker=ImagePicker();
+  firebase_storage.FirebaseStorage storage=firebase_storage.FirebaseStorage.instance;
+  File? file;
 
-  sendMessage(){
+  uploadImage(File? fileUrl)async{
+    DateTime time=DateTime.now();
+    firebase_storage.Reference ref=firebase_storage.FirebaseStorage.instance.ref('/notificationImage/'+time.microsecondsSinceEpoch.toString());
+    firebase_storage.UploadTask upload=ref.putFile(fileUrl!.absolute);
+    await Future.value(upload);
+    var newUrl=await ref.getDownloadURL();
+    sendMessage(newUrl.toString());
+  }
+
+
+  sendMessage(String message){
     DateTime dateTime=DateTime.now();
-    databaseReference.push().set({'key':dateTime.microsecondsSinceEpoch,'message':message.text}).then((value){print("sent message");}).catchError((error){
-      print("the error is ${error}");
-    });
+    if(message.isEmpty){
+      print("o hello");
+    }
+    else{
+      databaseReference.push().set({'key':dateTime.microsecondsSinceEpoch,'message':message,"date":DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime)}).catchError((error){
+        print("the error is ${error}");
+      });
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -49,35 +72,74 @@ class _NotificationScreenState extends State<NotificationScreen> {
       body: Column(
         children: [
           Expanded(flex: 8,
-            child:FirebaseAnimatedList(
-              reverse: false,
-              query:databaseReference,
-              itemBuilder: (context, snapshot, animation, index) {
-                if(!snapshot.exists){
-                  return CircularProgressIndicator();
-                }
-                else{
-                  return ListTile(
-                    title: Text(snapshot.child("message").value.toString()),
-                    subtitle: Text(snapshot.child("date").value.toString()),
+            child: Container(margin: EdgeInsets.all(10),
+              child: FirebaseAnimatedList(
+                query:databaseReference,
+                itemBuilder: (context, snapshot, animation, index) {
+                  return snapshot.child("message").value.toString().contains("https://firebasestorage.googleapis.com/v0/b/notifyme-59aae.appspot.com/o/notificationImage%")?Container(
+                      child: Column(
+                        children: [Image.network(snapshot.child("message").value.toString()),
+                          Text(snapshot.child("date").value.toString()),
+                        ],
+                      )
+                  ):Container(
+                    child: ListTile(
+                      title: Text(snapshot.child("message").value.toString()),
+                      subtitle: Text(snapshot.child("date").value.toString()),
+                    ),
                   );
-                }
-              },
+                },
+              ),
             ),
           ),
-          Expanded(flex: 1,
+          Expanded(flex: 2,
               child: Container(height: 40,
                 margin: EdgeInsets.only(left: 20,right: 20,),
-                child: TextField(
-                  controller: message,
-                  decoration: InputDecoration(suffixIcon: IconButton(onPressed: (){
-                    sendMessage();
-                    message.clear();
-                  },icon: Icon(Icons.send),),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25)
-                      )),
-                ),
+                child: Row(children: [
+                  Expanded(flex: 9,
+                    child: TextField(maxLines: 1,
+                      controller: messageController,
+                      decoration: InputDecoration(suffixIcon: IconButton(onPressed: (){
+                        showDialog(context: context, builder:(context) {
+                          return AlertDialog(title: Text("Pick the image from"),
+                            actions: [
+                              TextButton(onPressed: ()async{
+                                var image= await imagePicker.pickImage(source: ImageSource.gallery);
+                                if(image!=null){
+                                  file=File(image.path);
+                                  uploadImage(file);
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => NotificationScreen(),));
+                                }
+                                else{
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => NotificationScreen(),));
+                                }
+                              }, child: Text("Gallery")),
+                              TextButton(onPressed: ()async{
+                                var image=await imagePicker.pickImage(source: ImageSource.camera);
+                                if(image!=null){
+                                  file=File(image.path);
+                                  uploadImage(file);
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => NotificationScreen(),));
+                                }
+                                else{
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => NotificationScreen(),));
+                                }
+                              }, child: Text("camera")),
+                            ],
+                          );
+                        },);
+                      },icon: Icon(Icons.image),),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25)
+                          )),
+                    ),
+                  ),
+                  Expanded(flex: 1,
+                      child: IconButton(onPressed: (){
+                        sendMessage(messageController.text);
+                        messageController.clear();
+                      },icon: Icon(Icons.send),))
+                ],),
               )),
         ],
       ),
