7571f856d7a7103171ae9f39ded9f6dfb04249d3
USER <ranamusaib5102000@gmail.com>
2024-04-30 09:11:00 +0500

    add the logout button in the adminScreen

--- lib/screen/admindashBoard/addmindashboard.dart
+++ lib/screen/admindashBoard/addmindashboard.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notificationapp/screen/admindashBoard/notification.dart';
+import 'package:notificationapp/screen/login/loginscreen.dart';
 class AdminDashBoard extends StatefulWidget {
   const AdminDashBoard({Key? key}) : super(key: key);
 
@@ -23,7 +25,9 @@ class _appBarState extends State<AdminDashBoard> {
                 child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     IconButton(onPressed: (){
-                    }, icon:Icon(Icons.menu,color: Colors.white,)),
+                     FirebaseAuth.instance.signOut();
+                     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen(),));
+                    }, icon:Icon(Icons.logout,color: Colors.white,)),
                     CircleAvatar(child: Image(image: AssetImage("lib/assets/image/logo.png")),)
                   ],
                 ),
@@ -31,7 +35,7 @@ class _appBarState extends State<AdminDashBoard> {
               Container(margin: EdgeInsets.only(left: 40),
                   child: Text("DashBoard",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: Colors.white),)),
               Container(margin: EdgeInsets.only(left: 40),
-                  child: Text("Leatest update",style: TextStyle(fontSize: 20,color: Colors.white54),)),
+                  child: Text(FirebaseAuth.instance.currentUser!.email.toString(),style: TextStyle(fontSize: 20,color: Colors.white54),)),
             ],
           ),
           ),
